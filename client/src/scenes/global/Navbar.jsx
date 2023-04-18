b6089003e8db3322afe151dc795b72242dbf6f65
Jared Gutkin <jgutkinsam@gmail.com>
2023-04-17 22:22:08 -0500

    navbar added

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { Box, Badge, IconButton } from "@mui/material";
+import { PersonOutline, ShoppingBagOutlined, MenuOutlined, SearchOutlined} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
 
-export default Navbar
+  return (
+    <Box>
+      <Box>
+        <Box>
+          BAD MEI MEI
+        </Box>
+        <Box>
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+
+          <IconButton>
+            <PersonOutline />
+          </IconButton>
+          <Badge>
+            <IconButton>
+              <ShoppingBagOutlined />
+            </IconButton>
+          </Badge>
+          <IconButton>
+            <MenuOutlined />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Navbar;
