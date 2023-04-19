125579b7a757b648bd7a8ba055f427cd34545e63
Jared Gutkin <jgutkinsam@gmail.com>
2023-04-18 19:53:38 -0500

    navbar half done working on redux now

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Box, Badge, IconButton } from "@mui/material";
 import { PersonOutline, ShoppingBagOutlined, MenuOutlined, SearchOutlined} from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -9,31 +10,71 @@ const Navbar = () => {
   const cart = useSelector((state) => state.cart.cart);
 
   return (
-    <Box>
-      <Box>
-        <Box>
+    <Box
+      display="flex"
+      alignItems="center"
+      width="100%"
+      height="60px"
+      backgroundColor="rgba(255, 255, 255, 0.95)"
+      color="black"
+      position="fixed"
+      top="0"
+      left="0"
+      zIndex="1"
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          onClick={() => navigate("/")}
+          sx={{ "&:hover": { cursor: "pointer" } }}
+          color={shades.secondary[500]}
+        >
           BAD MEI MEI
         </Box>
-        <Box>
-          <IconButton>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          columnGap="20px"
+          zIndex="2"
+        >
+          <IconButton sx={{ color: "black" }}>
             <SearchOutlined />
           </IconButton>
-
-          <IconButton>
+          <IconButton sx={{ color: "black" }}>
             <PersonOutline />
           </IconButton>
-          <Badge>
-            <IconButton>
+          <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            invisible={cart.length === 0}
+            sx={{
+              "& .MuiBadge-badge": {
+                right: 5,
+                top: 5,
+                padding: "0 4px",
+                height: "14px",
+                minWidth: "13px",
+              },
+            }}
+          >
+            <IconButton
+
+            >
               <ShoppingBagOutlined />
             </IconButton>
           </Badge>
-          <IconButton>
+          <IconButton sx={{ color: "black" }}>
             <MenuOutlined />
           </IconButton>
         </Box>
       </Box>
     </Box>
   );
-};
+}
 
 export default Navbar;
